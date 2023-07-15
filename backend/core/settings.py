46f3eea14543f9da0adb540f061a6d42e39da07d
berzezek <wknduz@gmail.com>
2023-07-15 14:46:15 +0500

    add mysql db

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -66,14 +66,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'cn73530_animal',
+#         'USER': 'cn73530_animal',
+#         'PASSWORD': '!Aa20102010',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cn73530_animal',
-        'USER': 'cn73530_animal',
-        'PASSWORD': '!Aa20102010',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
